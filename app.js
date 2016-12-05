4ae225c944696f54d847d0f8c78c3712730632be
anicknam <a.nicknam@gmail.com>
2016-12-05 15:32:25 -0800

    add event listener for input boxes

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 
+var results = [[],[],[]];
 $(document).ready(function(){
-  var results = [];
-	console.log('hello');
+
+	$('.box').change(function(e){
+		var i = $(this).attr('id').split('-')[0];
+		var j = $(this).attr('id').split('-')[1];
+	  results[i][j] = e.target.value;
+		var status = checkWin(results);
+
+		// if status included a winner, show the winner
+		// if (status) {
+		// $('.winner').innerHTML(status)
+		// }
+	});
+
+  function checkWin() {
+  	// check all rows for all Xs or all Os
+
+  	// check all columns for all Xs or all Os 
+
+  	// check all diagonals for all Xs or all Os
+
+  	// if winner, return winner else return null
+
+  };
+  
 });
+
+
+
